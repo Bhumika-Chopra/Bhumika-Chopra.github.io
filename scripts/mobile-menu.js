77bed7f7b04a88fee914f30efa44739c81e11e19
Bhumika-Chopra <bhumikasunilchopra@gmail.com>
2024-10-07 23:56:54 -0500

    Clean up repo

--- scripts/mobile-menu.js
+++
@@ -1,6 +0,0 @@
-const mobileMenu = document.getElementById("mobileMenu");
-const menuToggler = document.getElementById("menuToggler");
-
-menuToggler.addEventListener("click", (e) => {
-  mobileMenu.classList.toggle("show");
-});
